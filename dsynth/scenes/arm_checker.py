9ea3d25f72e77597a422e811cde69e422a8c841f
mikkklyubbin <lyubin.misha@mail.ru>
2025-02-10 13:18:14 +0000

    arm_check

--- dsynth/scenes/arm_checker.py
+++ dsynth/scenes/arm_checker.py
@@ -22,18 +22,36 @@ from transforms3d import quaternions
 import random
 import string
 import argparse
-
-
-json_file_path = "myscene_4_3.json"
+from darkstore_env import get_arena_data, DarkstoreEnv
+#lubin/darkstore_synthesizer/scenes/myscene_5_5.json
+ENV_NAME = 'DarkstoreEnv'
+json_file_path = "./../../scenes/myscene_5_5.json"
 assets_dir = "darkstore_synthesizer/models"
 style_id = 0
 mapping_file = None
-
 with open(json_file_path, "r") as f: # big_scene , one_shelf_many_milk_scene , customize
     data = json.load(f)
+for el in data['meta']:
+    print(el)
 
-n = data['meta']['n']
-m = data['meta']['m']
-arena_data = get_arena_data(x_cells=n, y_cells=m, height=4)
+env = DarkstoreEnv(scene_json = json_file_path, assets_dir = "/home/soshin/lubin/darkstore_synthesizer/models", style_ids = [0], robot_uids = "panda", render_mode="rgb_array")
+env = RecordEpisode(
+        env,
+        f"./videos__style{style_id}", # the directory to save replay videos and trajectories to
+        # on GPU sim we record intervals, not by single episodes as there are multiple envs
+        # each 100 steps a new video is saved
+        max_steps_per_video=100
+    )
 
-env = gym.make(ENV_NAME, robot_uids='fetch', style_ids = [style_id], num_envs=1, render_mode="rgb_array", enable_shadow=True, **arena_data)
+# step through the environment with random actions
+obs, _ = env.reset()
+viewer = env.render()
+if isinstance(viewer, sapien.utils.Viewer):
+    viewer.paused = False
+env.render()
+for i in tqdm(range(100)):
+    action = env.action_space.sample()
+    obs, reward, terminated, truncated, info = env.step(torch.zeros_like(torch.from_numpy(action)))
+    env.render()
+    # env.render_human() # will render with a window if possible
+env.close()
