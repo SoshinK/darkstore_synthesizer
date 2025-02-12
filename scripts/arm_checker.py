c12d393e11bb70ad01960bfd06a1c753e686d72f
SoshinK <soshinkonstantinv@gmail.com>
2025-02-12 14:30:45 +0300

    Add motionplanning draft for pick box

--- scripts/arm_checker.py
+++ scripts/arm_checker.py
@@ -2,7 +2,7 @@ from mani_skill.envs.tasks.empty_env import EmptyEnv
 from mani_skill.envs.sapien_env import BaseEnv
 
 from typing import Dict
-
+import time
 from mani_skill.agents.robots.fetch import FETCH_WHEELS_COLLISION_BIT
 from mani_skill.utils.building.ground import build_ground
 from mani_skill.utils.registration import register_env
@@ -23,14 +23,14 @@ import random
 import string
 import argparse
 sys.path.append(".")
-from dsynth.scenes.mv_panda_arm import solve_by_coords
 from dsynth.scenes.darkstore_env import get_arena_data, DarkstoreEnv
 from dsynth.envs.pick_to_cart import PickToCart
+from dsynth.motionplanning.solvers.pick_box import solve
 #lubin/darkstore_synthesizer/scenes/myscene_5_5.json
 ENV_NAME = 'DarkstoreEnv'
-json_file_path = "/home/soshin/gritsaev/darkstore_synthesizer/scenes/myscene_2_2.json"
+json_file_path = "myscene_2_2.json"
 # json_file_path = "darkstore_synthesizer/scenes/myscene_5_5.json"
-assets_dir = "/home/soshin/gritsaev/darkstore_synthesizer/models"
+assets_dir = "models"
 style_id = 0
 mapping_file = None
 with open(json_file_path, "r") as f: # big_scene , one_shelf_many_milk_scene , customize
@@ -42,13 +42,36 @@ n = data['meta']['n']
 m = data['meta']['m']
 arena_data = get_arena_data(x_cells=n, y_cells=m, height=4)
 
-env = PickToCart(scene_json = json_file_path, assets_dir = "models", style_ids = [0], robot_uids = "panda", render_mode="rgb_array", control_mode="pd_joint_pos", **arena_data)
+# env = PickToCart(scene_json = json_file_path, assets_dir = "models", style_ids = [0], robot_uids = "panda", render_mode="rgb_array", control_mode="pd_joint_pos", **arena_data)
+env = gym.make('PickToCart', 
+        robot_uids='panda', 
+        scene_json = json_file_path,
+        assets_dir = assets_dir,
+        mapping_file = mapping_file,
+        style_ids = [style_id], 
+        num_envs=1, 
+        viewer_camera_configs={'shader_pack': 'default'}, 
+        control_mode="pd_joint_pos",
+        render_mode="rgb_array", 
+        enable_shadow=True, 
+        **arena_data)
+
+# env = RecordEpisode(
+#         env,
+#         f"./videos__style{style_id}", # the directory to save replay videos and trajectories to
+#         # on GPU sim we record intervals, not by single episodes as there are multiple envs
+#         # each 100 steps a new video is saved
+#         max_steps_per_video=100
+#     )
+new_traj_name = time.strftime("%Y%m%d_%H%M%S")
 env = RecordEpisode(
         env,
-        f"./videos__style{style_id}", # the directory to save replay videos and trajectories to
-        # on GPU sim we record intervals, not by single episodes as there are multiple envs
-        # each 100 steps a new video is saved
-        max_steps_per_video=100
+        output_dir= f"./videos_style_{style_id}_motionplanning",
+        trajectory_name=new_traj_name, save_video=True,
+        source_type="motionplanning",
+        source_desc="official motion planning solution",
+        video_fps=30,
+        save_on_reset=False
     )
 
 obs, _ = env.reset()
@@ -62,9 +85,10 @@ env.render()
 target = env.actors["objects"]["milk_1_1_0"][0]['actor']
 goal_pose = sapien.Pose([0.5, 0, 0.3])
 
-for i in tqdm(range(10)):
+for i in tqdm(range(2)):
+# while True:
     if target is not None and goal_pose is not None:
-        res = solve_by_coords(env, target, goal_pose)
+        res = solve(env, target, goal_pose, vis=False)
     else:
         action = env.action_space.sample()
     # action = env.action_space.sample()
